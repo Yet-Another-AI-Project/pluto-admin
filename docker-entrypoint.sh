95e6e1a1e1ee09f5e433e1cc34cc11689ff0951d
Meng Li <lm2343635@126.com>
2020-09-14 00:19:08 +0900

    Fix sed

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sed -i 's@VUE_APP_BASE_API@'"$VUE_APP_BASE_API"'@/g' /app/static/js/app.*.js
+sed -i 's@VUE_APP_BASE_API@'"$VUE_APP_BASE_API"'@g' /app/static/js/app.*.js
 cd /app && python3 -m http.server $PORT
